d794a5e1b4370f0195f42e705624d5a95aa6d7f9
Alice Pereira de Souza <49755010+AliceP09@users.noreply.github.com>
2023-12-05 20:40:36 -0300

    Loops tentativas (#1)
    * Adicionando o pacote de desafio II e adicionando novas condicionais no projeto inicial
    
    * Adicionando novo desafio parte III

--- logica-js-projeto-inicial-master/app.js
+++ logica-js-projeto-inicial-master/app.js
@@ -1,12 +1,26 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 9;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1; 
 
-console.log('Iniciando estrutuda de condição')
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Voce errou :(');
+console.log('Iniciando estrutuda de condição');
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto) {
+
+    chute = prompt('Escolha um número entre 1 e 10');
+
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        // tentativas = tentativas + 1;
+        tentativas ++;
+    }
 }
