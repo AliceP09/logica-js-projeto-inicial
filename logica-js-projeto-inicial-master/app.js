f2c5c320be1497251be5362bbde31b61a44af375
AliceP09 <alice.09pereira7@gmail.com>
2023-12-06 07:04:46 -0300

    Adicionando operador tenário e função de gerar números aleatórios Math.radom

--- logica-js-projeto-inicial-master/app.js
+++ logica-js-projeto-inicial-master/app.js
@@ -1,19 +1,15 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1; 
 
-console.log('Iniciando estrutuda de condição');
-
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-
-    chute = prompt('Escolha um número entre 1 e 10');
-
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
-        alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;
     } else {
         if (chute > numeroSecreto) {
             alert(`O número secreto é menor que ${chute}`);
@@ -24,3 +20,13 @@ while (chute != numeroSecreto) {
         tentativas ++;
     }
 }
+
+// Exemplo operador tenário             Verdadeiro      falso
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+// if (tentativas > 1) {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+// } else {
+//     alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+// }
